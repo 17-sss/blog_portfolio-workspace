9d417291202ab72b9c555b08f176698fe221a32e
Hoyoung Son <xzxking17@gmail.com>
2021-11-05 18:23:01 +0900

    chore: :gear: meta 데이터 수정 (gatsby-config.js)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `코딩하는 농사꾼 라노의 개발 블로그`,
+    title: `코딩 농사꾼 라노의 개발 블로그`,
     description: `꾸준함이라는 덕목이 최고의 미덕이라고 생각하는 주니어 개발자입니다.`,
     author: `Hoyoung Son <Rano>`,
     siteUrl: `https://17-sss.github.io/`,
