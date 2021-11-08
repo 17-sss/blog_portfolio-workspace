85ebda63107c65e0b1a4fee497eb913926d802df
Hoyoung Son <xzxking17@gmail.com>
2021-11-08 23:55:12 +0900

    feat: :sparkles: (portfolio) HomeSection 컴포넌트 완성

--- src/components/portfolio/Main/HomeSection/index.tsx
+++ src/components/portfolio/Main/HomeSection/index.tsx
@@ -1,8 +1,21 @@
-import { Section } from 'components/portfolio/Common';
-import { FunctionComponent } from 'react';
+import React, { FunctionComponent } from 'react';
+import { usePortfolioState } from 'utils/contexts/PortfolioContext';
+import WaveGraphic from '../WaveGraphic';
+import * as S from './style';
 
 const HomeSection: FunctionComponent = function ({ ...props }) {
-  return <Section {...props}></Section>;
+  const { waveImages } = usePortfolioState();
+
+  return (
+    <S.HomeSectionLayout {...props}>
+      <S.WaveGraphicBox>
+        <WaveGraphic waveImages={waveImages} />
+      </S.WaveGraphicBox>
+      <S.SectionInnerBox>
+        <S.IntroParagraph>Developer Hoyoung's portfolio.</S.IntroParagraph>
+      </S.SectionInnerBox>
+    </S.HomeSectionLayout>
+  );
 };
 
 export default HomeSection;
--- src/components/portfolio/Main/HomeSection/style.tsx
+++ src/components/portfolio/Main/HomeSection/style.tsx
@@ -1,5 +1,40 @@
 import styled from '@emotion/styled';
 
-export const HomeSectionLayout = styled.div`
-  min-height: 25vh;
+import { InnerContainer } from 'components/portfolio/Common';
+import { PORTFOLIO_HEADER } from 'utils/constants';
+import { flexSet } from 'utils/style';
+
+export const HomeSectionLayout = styled.section`
+  position: relative;
+`;
+
+export const WaveGraphicBox = styled.div`
+  height: ${`calc(100vh - ${PORTFOLIO_HEADER.height}px)`};
+  background-color: ${({ theme }) => theme.grayScaleColors.background};
+  opacity: 0.75;
+`;
+
+export const SectionInnerBox = styled(InnerContainer)`
+  ${flexSet({ alignItems: 'center', justifyContent: 'center', flexDirection: 'column' })}
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 18%;
+`;
+
+export const IntroParagraph = styled.p`
+  font-family: 'Nanum Myeongjo', serif, Noto Sans KR, sans-serif;
+  font-size: 3vw;
+  color: ${({ theme }) => theme.grayScaleColors.font};
+
+  animation: blink 1.5s ease-in-out infinite alternate;
+  @keyframes blink {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
 `;
