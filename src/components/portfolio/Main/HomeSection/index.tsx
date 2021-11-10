897df86f938f0bec0fd99c94729dd55d3135fb45
Hoyoung Son <xzxking17@gmail.com>
2021-11-10 13:59:24 +0900

    feat: :sparkles: (portfolio) HomeSection 부분 완성

--- src/components/portfolio/Main/HomeSection/index.tsx
+++ src/components/portfolio/Main/HomeSection/index.tsx
@@ -1,20 +1,18 @@
 import React, { FunctionComponent } from 'react';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import { PORTFOLIO_HOME_SECTION } from 'utils/constants';
-import { usePortfolioState } from 'utils/contexts/PortfolioContext';
-import WaveGraphic from '../WaveGraphic';
 import * as S from './style';
 
 const HomeSection: FunctionComponent = function ({ ...props }) {
-  const { waveImages } = usePortfolioState();
-
   return (
     <S.HomeSectionLayout {...props}>
-      <S.WaveGraphicBox>
-        <WaveGraphic waveImages={waveImages} />
-      </S.WaveGraphicBox>
-      <S.SectionInnerBox>
-        <S.IntroParagraph>{PORTFOLIO_HOME_SECTION.greetingText}</S.IntroParagraph>
-      </S.SectionInnerBox>
+      <S.IntroParagraph>{PORTFOLIO_HOME_SECTION.greetingText}</S.IntroParagraph>
+      <S.ScrollInfoBox>
+        <span>Scroll Down</span>
+        <S.ScrollDownButton>
+          <KeyboardArrowDownIcon />
+        </S.ScrollDownButton>
+      </S.ScrollInfoBox>
     </S.HomeSectionLayout>
   );
 };
--- src/components/portfolio/Main/HomeSection/style.tsx
+++ src/components/portfolio/Main/HomeSection/style.tsx
@@ -1,38 +1,21 @@
 import styled from '@emotion/styled';
-import { Typography } from '@material-ui/core';
-
-import { InnerContainer } from 'components/portfolio/Common';
-import { PORTFOLIO_HEADER } from 'utils/constants';
-import { flexSet, getMediaQueries } from 'utils/style';
+import { Button, Typography } from '@material-ui/core';
+import { flexSet, getMediaQueries, theme } from 'utils/style';
 
 export const HomeSectionLayout = styled.section`
-  position: relative;
-`;
-
-export const WaveGraphicBox = styled.div`
-  height: ${`calc(100vh - ${PORTFOLIO_HEADER.height}px)`};
-  background-color: ${({ theme }) => theme.grayScaleColors.background};
-  opacity: 0.75;
-`;
-
-export const SectionInnerBox = styled(InnerContainer)`
-  ${flexSet({ alignItems: 'center', justifyContent: 'center', flexDirection: 'column' })}
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 15%;
+  ${flexSet({ alignItems: 'center', justifyContent: 'center', flexDirection: 'column' })};
+  row-gap: 6vh;
+  height: 100%;
 `;
 
 export const IntroParagraph = styled(({ ...props }) => <Typography paragraph align="center" {...props} />)`
   font-family: 'Nanum Myeongjo', serif, Noto Sans KR, sans-serif;
   font-size: 3.3vw;
-  padding: 0 24px;
+  padding: 24px;
+
   ${getMediaQueries('mobile', true)} {
     font-size: 3.5vh;
   }
-
-  color: ${({ theme }) => theme.grayScaleColors.font};
 
   animation: blink 1.5s ease-in-out infinite alternate;
   @keyframes blink {
@@ -42,5 +25,30 @@ export const IntroParagraph = styled(({ ...props }) => <Typography paragraph ali
     100% {
       opacity: 1;
     }
+  }
+`;
+
+export const ScrollInfoBox = styled.div`
+  ${flexSet({ alignItems: 'center', justifyContent: 'center', flexDirection: 'column' })};
+
+  color: ${theme.grayScaleColors.font};
+  font-size: 2vh;
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  animation: fadeIn 2s;
+`;
+export const ScrollDownButton = styled(Button)`
+  padding: 0;
+  min-width: fit-content;
+  svg {
+    color: ${theme.grayScaleColors.font};
+    font-size: 4.5vh;
   }
 `;
