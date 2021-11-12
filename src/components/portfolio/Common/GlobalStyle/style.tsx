65f4cd9ba56bf10f20660bc5eac1caeeb6834c26
Hoyoung Son <xzxking17@gmail.com>
2021-11-12 18:20:08 +0900

    style: :art: Font 추가 (내용 참고) - 현재까지 적용된 폰트     - 우아한형제들 폰트: Do Hyeon (400)     - Nanum Myeongjo (400, 700, 800)     - Noto Sans Korean (100, 300, 400, 500, 700)     - Roboto (100, 300, 400, 500, 700) (+all italic)

--- src/components/portfolio/Common/GlobalStyle/style.tsx
+++ src/components/portfolio/Common/GlobalStyle/style.tsx
@@ -2,11 +2,11 @@ import { css, Theme } from '@emotion/react';
 import { resetStyle, theme } from 'utils/style';
 
 export const defaultStyle = ((theme: Theme) => css`
-  @import url('https://fonts.googleapis.com/css2?family=Do+Hyeon&family=Nanum+Myeongjo&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Do+Hyeon&family=Nanum+Myeongjo:wght@400;700&family=Noto+Sans+KR:wght@100;300;400;500;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500;1,700&display=swap');
 
   ${resetStyle};
   html {
     font-size: ${theme.fontSizes.root};
-    font-family: Noto Sans KR, sans-serif;
+    font-family: 'Noto Sans KR', 'Roboto', sans-serif, serif;
   }
 `)(theme);
