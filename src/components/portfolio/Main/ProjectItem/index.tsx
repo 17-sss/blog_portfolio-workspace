474ebe8cec35525075edfc2ee98c3c4a12cf0f8d
Hoyoung Son <xzxking17@gmail.com>
2021-11-17 19:44:47 +0900

    feat: :sparkles: (portfolio) ProjectItem 추가 - Carousel, Accordion등 적용 - ProejctSection내의 요소

--- src/components/portfolio/Main/ProjectItem/index.tsx
+++ src/components/portfolio/Main/ProjectItem/index.tsx
@@ -1,16 +1,14 @@
 import { FunctionComponent } from 'react';
+import { AccordionDetails } from '@material-ui/core';
 import { Paragraph } from 'components/portfolio/Common';
-import * as S from './style';
+import ImageCarousel from '../ImageCarousel';
 import { PortfolioMarkdownNode } from 'utils/types';
-
-import { Accordion, AccordionDetails } from '@material-ui/core';
+import * as S from './style';
+import { changeFirstCharUpperCase } from 'utils/functions';
 
 const ProjectItem: FunctionComponent<PortfolioMarkdownNode> = function (props) {
-  const {
-    frontmatter: { portfolioInfo },
-    html,
-  } = props;
-  const { title, duration, memberInfo, skills, images } = portfolioInfo;
+  const { frontmatter: { portfolioInfo }, html } = props;
+  const { title, subTitle, duration: { startDate, endDate }, memberInfo, skills, images, type } = portfolioInfo;
 
   return (
     <S.ProjectItemLayout>
@@ -18,11 +16,30 @@ const ProjectItem: FunctionComponent<PortfolioMarkdownNode> = function (props) {
         {title}
       </Paragraph>
       <S.ContentBox>
-        {/* title, m이외 */}
-        <div>이미지</div>
-        <div>내용</div>
-
-        {/* Content */}
+        {/* Images */}
+        <S.ImageBox>
+          <div className="inner">
+            <ImageCarousel title={title} images={images} />{' '}
+          </div>
+        </S.ImageBox>
+        {/* Info */}
+        <S.InfoBox>
+          <S.SubTitleText>{subTitle}</S.SubTitleText>
+          <S.MemberInfoText>{`(${memberInfo})`}</S.MemberInfoText>
+          <S.TextBox>
+            <S.NameText>기간</S.NameText>
+            <S.ValueText>{`${startDate} ~ ${endDate || 'ing'}`}</S.ValueText>
+          </S.TextBox>
+          <S.TextBox>
+            <S.NameText>사용 기술</S.NameText>
+            <S.ValueText>{skills.map((v, i) => <S.Code key={i}>{v}</S.Code>)}</S.ValueText>
+          </S.TextBox>
+          <S.TextBox>
+            <S.NameText>분류</S.NameText>
+            <S.TypeText isLibrary={type === 'library'}>{changeFirstCharUpperCase(type)}</S.TypeText>
+          </S.TextBox>
+        </S.InfoBox>
+        {/* Detail */}
         {html && (
           <S.AccordionBox elevation={0}>
             <S.AccordionOpenSummary />
--- src/components/portfolio/Main/ProjectItem/style.tsx
+++ src/components/portfolio/Main/ProjectItem/style.tsx
@@ -2,7 +2,8 @@ import styled from '@emotion/styled';
 import { Accordion, AccordionSummary, Card, CardContent } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { MarkdownRenderer } from 'components/common';
-import { getMediaQueries } from 'utils/style';
+import { Paragraph } from 'components/portfolio/Common';
+import { getMediaQueries, setFlex } from 'utils/style';
 
 export const ProjectItemLayout = styled(({ ...props }) => <Card component="li" elevation={2} {...props} />)`
   padding: 24px;
@@ -13,20 +14,25 @@ export const ProjectItemLayout = styled(({ ...props }) => <Card component="li" e
   }
 `;
 
+// [1] 전체 ContentBox =======================================
 export const ContentBox = styled(CardContent)`
-  display: grid;
-  grid-template-columns: repeat(2, calc(100% / 2));
-  margin-bottom: 16px;
-  padding: 0 6vh;
+  ${setFlex({ justifyContent: 'center', flexDirection: 'column' })}
 
-  &:last-of-type {
-    padding-bottom: 0;
-  }
+  ${getMediaQueries({ type: 'desktop' })} {
+    display: grid;
+    grid-template-columns: repeat(2, calc(100% / 2));
+    margin-bottom: 16px;
+    padding: 0 6vh;
 
-  & > :last-of-type {
-    /* ContentBox 안의 마지막 자식 (Accordion 컴포넌트)  */
-    grid-column-start: 1;
-    grid-column-end: 3;
+    &:last-of-type {
+      padding-bottom: 0;
+    }
+
+    & > :last-of-type {
+      /* ContentBox 안의 마지막 자식 (Accordion 컴포넌트)  */
+      grid-column-start: 1;
+      grid-column-end: 3;
+    }
   }
 
   ${getMediaQueries({ type: 'mobile' })} {
@@ -34,6 +40,117 @@ export const ContentBox = styled(CardContent)`
   }
 `;
 
+// [2] Carousel이 들어갈 ImageBox (inner)에 들어감 =======================================
+export const ImageBox = styled.div`
+  ${setFlex({ justifyContent: 'center', alignItems: 'center' })}
+  padding: 24px;
+
+  .inner {
+    ${getMediaQueries({ type: 'tablet' })} {
+      max-width: 80%;
+    }
+
+    ${getMediaQueries({ type: 'mobile' })} {
+      max-width: 90%;
+    }
+  }
+`;
+
+// [3] 텍스트 정보들이 들어갈 InfoBox =======================================
+export const InfoBox = styled.div`
+  ${setFlex({ justifyContent: 'center', alignItems: 'flex-start', flexDirection: 'column' })}
+  padding: 12px;
+
+  ${getMediaQueries({ type: 'tablet', customWidth: { minWidth: 0 } })} {
+    min-width: 75%;
+    margin: 0 auto;
+  }
+`;
+
+const CommonParagraph = styled(({ ...props }) => <Paragraph {...props} />)`
+  ${setFlex({ justifyContent: 'flex-start', alignItems: 'center' })};
+  flex-wrap: wrap;
+`;
+const SubParagraph = styled(CommonParagraph)`
+  color: ${({ theme }) => theme.grayScaleColors.font};
+  font-weight: 700;
+  padding: 0;
+`;
+
+// 1) subTitle 텍스트
+export const SubTitleText = styled(SubParagraph)`
+  font-size: ${({ theme }) => theme.fontSizes['18']};
+  margin-bottom: 4px;
+
+  ${getMediaQueries({ type: 'mobile' })} {
+    font-size: ${({ theme }) => theme.fontSizes['16']};
+  }
+`;
+
+// 2) memberInfo 텍스트
+export const MemberInfoText = styled(SubParagraph)`
+  font-size: ${({ theme }) => theme.fontSizes['16']};
+  margin-bottom: 12px;
+
+  ${getMediaQueries({ type: 'mobile' })} {
+    font-size: ${({ theme }) => theme.fontSizes['14']};
+  }
+`;
+
+// 3) duration, skills, type 텍스트가 들어갈 TextBox
+export const TextBox = styled.div`
+  ${setFlex({ alignItems: 'center' })};
+  column-gap: 8px;
+  & + & {
+    margin-top: 8px;
+  }
+`;
+
+// 3-1) duration, skills 텍스트 전용
+export const NameText = styled(CommonParagraph)`
+  font-size: ${({ theme }) => theme.fontSizes['16']};
+  font-weight: 600;
+  color: ${({ theme }) => theme.grayScaleColors.font};
+  padding: 0;
+  margin-bottom: 0;
+
+  ${getMediaQueries({ type: 'mobile' })} {
+    font-size: ${({ theme }) => theme.fontSizes['14']};
+  }
+`;
+
+export const ValueText = styled(NameText)`
+  font-weight: 400;
+  &.type {
+    color: ${({ theme }) => theme.grayScaleColors.lightFont};
+  }
+`;
+
+// 3-1-1) skills의 item들 디자인
+export const Code = styled.code`
+  font-family: 'SFMono-Regular', Menlo, Consolas, 'PT Mono', 'Liberation Mono', Courier, monospace;
+  line-height: normal;
+  background: rgba(135, 131, 120, 0.15);
+  color: #eb5757;
+  border-radius: 3px;
+  font-size: 85%;
+  padding: 0.2em 0.4em;
+
+  & + & {
+    margin-left: 4px;
+  }
+`;
+
+// --
+
+// 3-2) type 텍스트 전용
+type TypeTextProps = { isLibrary?: boolean };
+export const TypeText = styled(({ isLibrary, ...props }: TypeTextProps) => <ValueText {...props} />)`
+  font-weight: 600;
+  color: ${({ theme, isLibrary }) => (isLibrary ? theme.colors.basicGreen : theme.colors.darkBlue)};
+`;
+
+// [4] 마크다운의 상세 설명이 들어갈 AccordionBox =======================================
 export const AccordionBox = styled(Accordion)`
   &.Mui-expanded {
     margin: 0;
