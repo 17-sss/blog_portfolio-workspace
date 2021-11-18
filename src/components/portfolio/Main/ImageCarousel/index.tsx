ec13d9608c825d488859819ddf2ef9844071aaef
Hoyoung Son <xzxking17@gmail.com>
2021-11-18 16:11:18 +0900

    refactor: :hammer: (portfolio) ImageCarousel 렌더링 방식 변경

--- src/components/portfolio/Main/ImageCarousel/index.tsx
+++ src/components/portfolio/Main/ImageCarousel/index.tsx
@@ -9,15 +9,9 @@ type ImageCarouselProps = { title: string; images: PortfolioImage[] };
 const ImageCarousel: FunctionComponent<ImageCarouselProps> = function ({ title, images }) {
   const carouselImages = useMemo(() => {
     if (!images || !images.length) return;
-    return images.map(({ childImageSharp, publicURL }, i) => {
-      if (!childImageSharp && !publicURL) return;
-      const gatsbyImageData = childImageSharp?.gatsbyImageData;
-      const alt = `${title} : ${i}`;
-
-      // 이 방법이 아닌, 맨 하단에 있는 방법 나중에 적용해보기. Carousel이 video태그만.. 이상하게 돌아감
-      if (gatsbyImageData) return <S.CarouselGatsbyImage key={i} alt={alt} image={gatsbyImageData} />;
-      return <S.Image src={publicURL} alt={alt} width={'100%'} height={'100%'} />;
-    });
+    return images.map(({ publicURL }, i) => (
+      <S.Image key={i} src={publicURL} alt={`${title} : ${i}`} width={'100%'} height={'100%'} />
+    ));
   }, [images]);
 
   const carouselProps: TCarouselProps = {
@@ -37,30 +31,52 @@ const ImageCarousel: FunctionComponent<ImageCarouselProps> = function ({ title, 
 export default ImageCarousel;
 
 /* 
-const ImageCarousel: FunctionComponent<ImageCarouselProps> = function ({ title, images }) {
-  const carouselImages = useMemo(() => {
-    if (!images || !images.length) return;
-    return images.map(({ childImageSharp, publicURL }, i) => {
-      if (!childImageSharp && !publicURL) return;
-      const gatsbyImageData = childImageSharp?.gatsbyImageData;
-      const alt = `${title} : ${i}`;
 
-      if (gatsbyImageData) return <S.CarouselGatsbyImage key={i} alt={alt} image={gatsbyImageData} />;
+// [1] 일반 이미지 적용
+const carouselImages = useMemo(() => {
+  if (!images || !images.length) return;
+  return images.map(({ publicURL }, i) => (
+    <S.Image key={i} src={publicURL} alt={`${title} : ${i}`} width={'100%'} height={'100%'} />
+  ));
+}, [images]);
 
-      const allowExtenstion = ['mp4', 'webm'];
-      const extension = getExtension(publicURL)?.slice(1) ?? '';
-      if (allowExtenstion.includes(extension.toLowerCase())) {
-        return (
-          // 방법 1 (이렇게 해야하는데..)
-          // 현재 source를 넣어서 처리하면 원활하게 동작하지 않음..
-          <video autoPlay loop muted playsInline src={publicURL}>
-            <source src={publicURL} type={`video/${extension}`} />
-          </video>
-          // 방법 2 (잘 동작하나 이동시 깜박임)
-          <video autoPlay loop muted playsInline src={publicURL} width="100%" height="100%"/>
-        );
-      }
-      return <S.Image src={publicURL} alt={alt} width={'100%'} height={'100%'} />;
-    });
-  }, [images]);
+// [2] Gatsby 이미지 적용 -- 버벅임.
+const carouselImages = useMemo(() => {
+  if (!images || !images.length) return;
+  return images.map(({ childImageSharp, publicURL }, i) => {
+    if (!childImageSharp && !publicURL) return;
+    const gatsbyImageData = childImageSharp?.gatsbyImageData;
+    const alt = `${title} : ${i}`;
+
+    if (gatsbyImageData) return <S.CarouselGatsbyImage key={i} alt={alt} image={gatsbyImageData} />;
+    return <S.Image src={publicURL} alt={alt} width={'100%'} height={'100%'} />;
+  });
+}, [images]);
+
+// [3] 동영상 적용 -- 이걸 적용해야 하는데..
+const carouselImages = useMemo(() => {
+  if (!images || !images.length) return;
+  return images.map(({ childImageSharp, publicURL }, i) => {
+    if (!childImageSharp && !publicURL) return;
+    const gatsbyImageData = childImageSharp?.gatsbyImageData;
+    const alt = `${title} : ${i}`;
+
+    if (gatsbyImageData) return <S.CarouselGatsbyImage key={i} alt={alt} image={gatsbyImageData} />;
+
+    const allowExtenstion = ['mp4', 'webm'];
+    const extension = getExtension(publicURL)?.slice(1) ?? '';
+    if (allowExtenstion.includes(extension.toLowerCase())) {
+      return (
+        // 방법 1 (이렇게 해야하는데..)
+        // 현재 source를 넣어서 처리하면 원활하게 동작하지 않음..
+        <video autoPlay loop muted playsInline src={publicURL}>
+          <source src={publicURL} type={`video/${extension}`} />
+        </video>
+        // 방법 2 (잘 동작하나 이동시 깜박임)
+        <video autoPlay loop muted playsInline src={publicURL} width="100%" height="100%"/>
+      );
+    }
+    return <S.Image src={publicURL} alt={alt} width={'100%'} height={'100%'} />;
+  });
+}, [images]);
 */
